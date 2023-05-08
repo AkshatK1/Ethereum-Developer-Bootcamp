0cbefd5d96ce4f47135882ddf7dbc83ca4d99118
Akshat Khandelwal <khandelwalakshat46@gmail.com>
2023-05-08 23:50:15 +0530

    modified app to take privateKey instead of address

--- week 1 project/ecdsa-node/client/src/Wallet.jsx
+++ week 1 project/ecdsa-node/client/src/Wallet.jsx
@@ -1,8 +1,14 @@
 import server from "./server";
+import * as secp from "ethereum-cryptography/secp256k1"; // const secp = require("ethereum-cryptography/secp256k1");
+import { toHex } from "ethereum-cryptography/utils"; // const { toHex } = require("ethereum-cryptography/utils");
+import { keccak256 } from "ethereum-cryptography/keccak"; // const { keccak256 } = require("ethereum-cryptography/keccak");
 
-function Wallet({ address, setAddress, balance, setBalance }) {
+function Wallet({ address, setAddress, balance, setBalance, privateKey, setPrivateKey }) {
   async function onChange(evt) {
-    const address = evt.target.value;
+    const privateKey = evt.target.value;
+    setPrivateKey(privateKey);
+    const publicKey = secp.secp256k1.getPublicKey(privateKey);
+    const address = getAddress(publicKey);
     setAddress(address);
     if (address) {
       const {
@@ -19,13 +25,27 @@ function Wallet({ address, setAddress, balance, setBalance }) {
       <h1>Your Wallet</h1>
 
       <label>
-        Wallet Address
-        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
+        Private Key
+        <input placeholder="Type your private key" value={privateKey} onChange={onChange}></input>
+      </label>
+      <label>
+        Address:
+        <p>
+          {address}
+        </p>
       </label>
 
       <div className="balance">Balance: {balance}</div>
     </div>
   );
+}
+
+function getAddress(publicKey) {
+  // get Address similar to Ethereum
+  const oneByteOff = publicKey.slice(1);
+  const keccak256hash = keccak256(oneByteOff);
+  const ethKey = '0x' + toHex(keccak256hash.slice(-20));
+  return ethKey;
 }
 
 export default Wallet;
