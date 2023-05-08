b35c01cc06c50c07bc155d3f8c1676cf2039c9ba
Akshat Khandelwal <khandelwalakshat46@gmail.com>
2023-05-08 23:08:57 +0530

    changed public keys to the ones generated

--- week 1 project/ecdsa-node/server/index.js
+++ week 1 project/ecdsa-node/server/index.js
@@ -7,9 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "0x57ca3d7807553e87dd1c747a71d00bd187c6ffb4": 100, // Akshat - 6a08c3be17c503929b43d86743dc36dbdfe622f64349d4dc8b95dc0924206984
+  "0xd4c8eabbf6f7417229752b813c7e77f290bfc1fe": 50, // Aslam - f945c495bf606a2621a88c4724f824b4b0201906760fa947c693d52b776e93ae
+  "0x547a0747135a787561a8ba1f2a7e21c36d0bd54c": 75, // Anurag - dbb52df1fee09e5f8f088ed2ef6398034847faae7860a389f06de7383828b2d3
 };
 
 app.get("/balance/:address", (req, res) => {
