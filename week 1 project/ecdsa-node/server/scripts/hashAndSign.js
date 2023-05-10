82a9682014128233cb2f47f5c143e80dea50a97f
Akshat Khandelwal <khandelwalakshat46@gmail.com>
2023-05-11 02:26:12 +0530

    changed message object properties

--- week 1 project/ecdsa-node/server/scripts/hashAndSign.js
+++ week 1 project/ecdsa-node/server/scripts/hashAndSign.js
@@ -2,13 +2,14 @@ const secp = require("ethereum-cryptography/secp256k1");
 const { toHex, utf8ToBytes } = require("ethereum-cryptography/utils");
 const { keccak256 } = require("ethereum-cryptography/keccak");
 
-const PRIVATE_KEY = "a7de947171dbcc9c59d66e57ee79320c573ccaa775a5818ed923ec1c5a32b003";
-const ADDRESS = "0x185f97d99923b78d256d8104bda6bed2a55cb57c";
+const PRIVATE_KEY /* OF SENDER */ = "a7de947171dbcc9c59d66e57ee79320c573ccaa775a5818ed923ec1c5a32b003";
+const FROM_ADDRESS = "0xd07a9f37aa4457b019ca0d31a8ce7c0acf24b8c2";
+const TO_ADDRESS = "0x185f97d99923b78d256d8104bda6bed2a55cb57c";
 const AMOUNT = 10;
 
 const message = {
-    sender: PRIVATE_KEY,
-    recipient: ADDRESS,
+    from: FROM_ADDRESS,
+    to: TO_ADDRESS,
     amount: AMOUNT
 };
 
